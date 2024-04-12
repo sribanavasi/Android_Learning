7b93b1b4e97c4a227ae5f5ddf72313868de5aa89
sribanavasi <115912497+sribanavasi@users.noreply.github.com>
2024-04-12 11:27:18 +0530

    Update MainActivity.kt

--- 3.Activity and its lifecycle/ActivityLifeCycle/app/src/main/java/com/appAdda/activitylifecycle/MainActivity.kt
+++ 3.Activity and its lifecycle/ActivityLifeCycle/app/src/main/java/com/appAdda/activitylifecycle/MainActivity.kt
@@ -15,7 +15,7 @@ import android.widget.Button
  * Activity B : onStop(),onDestroy()**/
 class MainActivity : AppCompatActivity() {
     lateinit var button: Button
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) { //In this state, perform basic activity setup logic, that happens, only once for an activity. example: set layout/view of activity (setContentView()). restore state from the bundle, if it ever existed before. initialize member views, if it require later.
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("onCreate Called Activity 1")
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
         //this.finish()//if you call finish here, state goes to  on Destroy
     }
 
-    override fun onStart() {
+    override fun onStart() {//
         super.onStart()
         println("onStart Called Activity 1")
         //this.finish()//if you call finish here, state goes to  on, on stop then on Destroy
@@ -39,24 +39,25 @@ class MainActivity : AppCompatActivity() {
         println("onRestart Called Activity 1")
     }
 
-    override fun onResume() {
+    override fun onResume() { //Common practice is to start resource-dependent operations like opening cameras or sensors-related operations are done in this state. (You can also start these in on create, then the system denies camera/resource access to other activities that or active)
         super.onResume()
         println("onResume Called Activity 1")
 //        this.finish()//if you call finish here, state goes to  on, on Pause, on stop then on Destroy
 
     }
 
-    override fun onPause() {
+    override fun onPause() {//Common practice is to release system resources in this state. Don't use it to save application data/ network calls, because, this function is quick and may not complete those operations.
         super.onPause()
         println("onPause Called Activity 1")
     }
 
-    override fun onStop() {
+    override fun onStop() {//perform cpu intensive, shutdown operation like, ex: saving data to database if not saved before.
+
         super.onStop()
         println("onStop Called Activity 1")
     }
 
-    override fun onDestroy() {
+    override fun onDestroy() {//Release and clean up all the resources occupied by activity by activitys.
         super.onDestroy()
         println("onDestroy Called Activity 1")
     }
